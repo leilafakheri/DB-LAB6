0ecb2a12d5b26b0f1fa7f8998a4278f07af249fa
leila <fakheri90@gmail.com>
2020-12-21 19:52:02 +0330

    app changed

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import {BooksService} from './books.service';
+import CreateBookDto from './dto/create-book.dto';
 
 @Controller('books')
-export class BooksController {}
+export default class BookController {
+  constructor(private readonly booksService: BooksService) {}
+  @Post('post')
+  postBook( @Body() book: CreateBookDto) {
+    return this.booksService.insert(book);
+  }
+  @Get()
+  getAll() {
+    return this.booksService.getAllBooks();
+  }
+}
